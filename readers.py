9bebcf50b4f5a4fe2718f75ad784fd57fd3db0cf
smart-life-tech <christlightworld@gmail.com>
2024-07-21 18:51:51 +0100

    modified:   readers.py

--- readers.py
+++ readers.py
@@ -1,5 +1,5 @@
 from time import sleep
-
+from smartcard.util import toHexString
 from smartcard.ReaderMonitoring import ReaderMonitor, ReaderObserver
 
 
@@ -13,6 +13,12 @@ class printobserver(ReaderObserver):
         (addedreaders, removedreaders) = actions
         print("Added readers", addedreaders)
         print("Removed readers", removedreaders)
+        # Use the first available reader
+        reader = addedreaders[0]
+        connection = reader.createConnection()
+        connection.connect()
+        print("Reader:", reader)
+        print("Card ATR:", toHexString(connection.getATR()))
 
 if __name__ == '__main__':
     print("Add or remove a smartcard reader to the system.")
