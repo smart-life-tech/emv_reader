be11dd8186980964b9ebb58da86c8545e9610cc8
smart-life-tech <christlightworld@gmail.com>
2024-08-05 15:56:15 +0100

    modified:   sc.py

--- sc.py
+++ sc.py
@@ -1,11 +1,35 @@
-import pyautogui
-import pytesseract
-from PIL import Image
+import subprocess
 import time
+import pyautogui
+from PIL import Image
+import pytesseract
+
+# Function to minimize all windows except Chrome
+def minimize_except_chrome():
+    # Get the list of all window IDs
+    windows = subprocess.check_output(['wmctrl', '-l']).decode('utf-8').splitlines()
+    chrome_windows = [line for line in windows if 'Google Chrome' in line]
+    all_windows = [line.split()[0] for line in windows]
+
+    # Minimize all windows
+    for win_id in all_windows:
+        subprocess.call(['wmctrl', '-i', '-a', win_id])  # Activate the window
+        subprocess.call(['wmctrl', '-i', '-b', 'add,hidden', win_id])  # Minimize the window
+
+    # Restore Chrome windows
+    for chrome_win in chrome_windows:
+        chrome_win_id = chrome_win.split()[0]
+        subprocess.call(['wmctrl', '-i', '-a', chrome_win_id])  # Activate the window
+        subprocess.call(['wmctrl', '-i', '-b', 'remove,hidden', chrome_win_id])  # Restore the window
+
+# Minimize all windows except Chrome
+minimize_except_chrome()
+
+# Give it a moment to minimize and restore windows
+time.sleep(3)
+
 # Take a screenshot of the current screen
 screenshot_path = 'screenshot.png'
-print("taking screen shot")
-time.sleep(5)
 screenshot = pyautogui.screenshot()
 screenshot.save(screenshot_path)
 
