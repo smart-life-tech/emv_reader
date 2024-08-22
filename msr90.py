4c455703e7ddd099d726dfa9e8a52f77a089b1a8
smart-life-tech <christlightworld@gmail.com>
2024-08-22 20:52:39 +0100

    modified:   msr90.py

--- msr90.py
+++ msr90.py
@@ -1,6 +1,7 @@
 import usb.core
 import usb.util
 import time
+
 # Find the MSR90 device
 dev = usb.core.find(idVendor=0xc216, idProduct=0x0180)
 
@@ -18,19 +19,34 @@ dev.set_configuration()
 # Get the endpoint for reading
 cfg = dev.get_active_configuration()
 intf = cfg[(0, 0)]
-
 endpoint = intf[0]
+expected_length = True
 while True:
     # Read data from the device
     time.sleep(2)
-    print("reading data")
+    print("Reading data")
+    data_chunks = []
     try:
         while True:
             data = dev.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
-            # Process the data as needed
-            print('Data read:', data)
+            if data:
+                data_chunks.append(data)
+                # Check if the data is complete (e.g., based on expected length or specific end byte)
+                if  expected_length:  # Replace with actual condition
+                    break
+            else:
+                break
+
+        # Process the complete data
+        full_data = b''.join(data_chunks)
+        print('Complete data read:', full_data)
 
     except usb.core.USBError as e:
-        print(e)
+        expected_length=False
         if e.args == ('Operation timed out',):
             print("Timed out")
+        else:
+            print("USB Error:", e)
+
+    except Exception as ex:
+        print("An error occurred:", ex)
