e5336c6b2cea2b634532e69f7c90bd6bb0a5253f
smart-life-tech <christlightworld@gmail.com>
2024-08-30 20:12:41 +0100

    modified:   readers.py

--- readers.py
+++ readers.py
@@ -6,31 +6,32 @@ dev = usb.core.find(idVendor=0x0801, idProduct=0x0005)
 
 if dev is None:
     raise ValueError("Device not found")
-print(dev)
-# Detach the device from the kernel driver
+
+# Detach the device from the kernel driver if necessary
 if dev.is_kernel_driver_active(0):
-    print("kenel is active")
     dev.detach_kernel_driver(0)
 
 # Set the configuration
 dev.set_configuration()
 
-# Claim the interface
-cfg = dev.get_active_configuration()
-intf = cfg[(0, 0)]
+# Claim the first interface
+usb.util.claim_interface(dev, 0)
 
-# Find the endpoint
-ep_in = usb.util.find_descriptor(
-    intf,
-    custom_match=lambda e: usb.util.endpoint_direction(e.bEndpointAddress) == usb.util.ENDPOINT_IN
-)
+# Endpoint addresses for reading data
+endpoint_in = dev[0][(0, 0)][1]  # IN endpoint address
+endpoint_out = dev[0][(0, 0)][0]  # OUT endpoint address
 
-if ep_in is None:
-    raise ValueError("IN Endpoint not found")
+# Read data from the device
+try:
+    # You may need to send some data to initialize communication
+    # dev.write(endpoint_out.bEndpointAddress, some_data)
 
-# Read data from the IN endpoint
-data = ep_in.read(ep_in.wMaxPacketSize)
-print("Data received:", data)
-
-# Release the interface
-usb.util.release_interface(dev, intf)
+    data = dev.read(endpoint_in.bEndpointAddress, endpoint_in.wMaxPacketSize)
+    print("Data received:", data)
+except usb.core.USBError as e:
+    print("Error reading from the device:", str(e))
+finally:
+    # Release the interface
+    usb.util.release_interface(dev, 0)
+    # Reattach the kernel driver
+    dev.attach_kernel_driver(0)
