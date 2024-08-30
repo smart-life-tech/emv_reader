4790b276860e36ae143c49dc4981b32c513efcd8
smart-life-tech <christlightworld@gmail.com>
2024-08-30 20:20:36 +0100

    modified:   readers.py

--- readers.py
+++ readers.py
@@ -1,6 +1,7 @@
 import usb.core
 import usb.util
 import time
+
 # Find the device
 dev = usb.core.find(idVendor=0x0801, idProduct=0x0005)
 
@@ -19,20 +20,25 @@ usb.util.claim_interface(dev, 0)
 
 # Endpoint addresses for reading data
 endpoint_in = dev[0][(0, 0)][1]  # IN endpoint address
-endpoint_out = dev[0][(0, 0)][0]  # OUT endpoint address
-while True:
-    time.sleep(3)
-    # Read data from the device
-    try:
-        # You may need to send some data to initialize communication
-        # dev.write(endpoint_out.bEndpointAddress, some_data)
 
-        data = dev.read(endpoint_in.bEndpointAddress, endpoint_in.wMaxPacketSize)
-        print("Data received:", data)
-    except usb.core.USBError as e:
-        print("Error reading from the device:", str(e))
-    finally:
-        # Release the interface
-        usb.util.release_interface(dev, 0)
-        # Reattach the kernel driver
+try:
+    while True:
+        time.sleep(3)  # Adjust sleep time as needed
+        
+        # Read data from the device
+        try:
+            data = dev.read(endpoint_in.bEndpointAddress, endpoint_in.wMaxPacketSize, timeout=5000)  # 5-second timeout
+            print("Data received:", data)
+        except usb.core.USBError as e:
+            if e.errno == 110:  # Timeout error
+                print("Timeout: No data received")
+            else:
+                print("Error reading from the device:", str(e))
+except KeyboardInterrupt:
+    print("Exiting...")
+
+finally:
+    # Release the interface and reattach the kernel driver when done
+    usb.util.release_interface(dev, 0)
+    if dev.is_kernel_driver_active(0) == False:
         dev.attach_kernel_driver(0)
