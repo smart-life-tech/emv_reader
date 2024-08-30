15b5828f5fb301a2b86134b062af438b14f07fb2
smart-life-tech <christlightworld@gmail.com>
2024-08-30 20:24:52 +0100

    modified:   readers.py

--- readers.py
+++ readers.py
@@ -9,36 +9,36 @@ if dev is None:
     raise ValueError("Device not found")
 
 # Detach the device from the kernel driver if necessary
-if dev.is_kernel_driver_active(0):
-    dev.detach_kernel_driver(0)
+if dev.is_kernel_driver_active(1):
+    dev.detach_kernel_driver(1)
 
 # Set the configuration
-#dev.set_configuration()
+dev.set_configuration()
 
-# Claim the first interface
-usb.util.claim_interface(dev, 0)
+# Claim the EMV interface (interface 1)
+usb.util.claim_interface(dev, 1)
 
-# Endpoint addresses for reading data
-endpoint_in = dev[0][(0, 0)][1]  # IN endpoint address
+# Endpoint addresses for EMV reader
+endpoint_in = dev[0][(1, 0)][0]  # IN endpoint for interface 1
 
 try:
     while True:
         time.sleep(3)  # Adjust sleep time as needed
         
-        # Read data from the device
+        # Read data from the EMV reader
         try:
             data = dev.read(endpoint_in.bEndpointAddress, endpoint_in.wMaxPacketSize, timeout=5000)  # 5-second timeout
-            print("Data received:", data)
+            print("EMV Data received:", data)
         except usb.core.USBError as e:
             if e.errno == 110:  # Timeout error
-                print("Timeout: No data received")
+                print("Timeout: No EMV data received")
             else:
-                print("Error reading from the device:", str(e))
+                print("Error reading from the EMV reader:", str(e))
 except KeyboardInterrupt:
     print("Exiting...")
 
 finally:
     # Release the interface and reattach the kernel driver when done
-    usb.util.release_interface(dev, 0)
-    if dev.is_kernel_driver_active(0) == False:
-        dev.attach_kernel_driver(0)
+    usb.util.release_interface(dev, 1)
+    if dev.is_kernel_driver_active(1) == False:
+        dev.attach_kernel_driver(1)
