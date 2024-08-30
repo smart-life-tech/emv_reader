a7d36f03cce9ecd366537bc6992585e47a0f13ea
smart-life-tech <christlightworld@gmail.com>
2024-08-30 16:01:13 +0100

    modified:   test_brn.py

--- test_brn.py
+++ test_brn.py
@@ -1,4 +1,6 @@
 import os
+#sudo mkdir -p /root/.hidden_dir
+#echo -e "pos_id: 1\nbrn: add5008577937ea6a3227b496eda41f92fb8630db42639efb89197cefb4e77a5617b766ddc1ab5da" | sudo tee /root/.hidden_dir/secret_file.txt > /dev/null
 
 # Define the path to the secret file
 secret_file_path = os.path.expanduser("~/.hidden_dir/secret_file.txt")
