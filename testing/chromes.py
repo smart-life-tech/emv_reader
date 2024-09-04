1c9c633a21a47573c7d95b2d8c9c8893fa6f9efe
smart-life-tech <christlightworld@gmail.com>
2024-09-04 10:56:39 +0100

    modified:   testing/chromes.py

--- testing/chromes.py
+++ testing/chromes.py
@@ -1,6 +1,9 @@
 import pychrome
+import time
+import os
 print("starting")
 #i need to run this at every start  chromium-browser --remote-debugging-port=9222
+print("Starting Chrome interaction")
 try:
     # Connect to the Chromium browser
     browser = pychrome.Browser(url="http://127.0.0.1:9222")
@@ -12,17 +15,29 @@ try:
 
     tab = tabs[0]
     tab.start()
+    # Define the path to the secret file
+    secret_file_path = os.path.expanduser("~/.hidden_dir/secret_file.txt")
 
-    # Define the JavaScript code to trigger the click
-    js_code = """
-    document.getElementById('insert_card').click();
+    # Read the file contents
+    with open(secret_file_path, 'r') as file:
+        secret_data = file.read()
+
+    # Split the secret data into pos_id and brn
+    secret_lines = secret_data.strip().splitlines()
+    pos_id = secret_lines[0].split(': ')[1]
+    brn = secret_lines[1].split(': ')[1]
+    print(brn)
+    print(pos_id)
+
+    # JavaScript code to trigger the card check with simulated card data
+    js_code = f"""
+    window.emvProcessed("{'card_data'}", "{'type'}", "{'pos_id'}", "{'brn'}");
     """
-
     # Execute the JavaScript code
     result = tab.Runtime.evaluate(expression=js_code)
-    print(result)
-
-    # Close the tab connection
-    #tab.stop()
+    print("JavaScript executed:", result)
+    
+    # Optional: Close the tab connection
+    time.sleep(1)
 except Exception as e:
     print(f"An error occurred: {e}")
