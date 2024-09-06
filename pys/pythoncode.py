8d3d24f6c0acdab6482909670208357d79367461
smart-life-tech <christlightworld@gmail.com>
2024-09-06 12:47:42 +0100

    modified:   pys/pythoncode.py

--- pys/pythoncode.py
+++ pys/pythoncode.py
@@ -26,5 +26,24 @@ def write_file():
     else:
         return jsonify({"status": "error", "message": "No content to write"}), 400
 
+@app.route('/api/reboot', methods=['POST'])
+def reboot():
+    if 1:
+        print("rebooted")
+        os.system('sudo reboot')
+        return jsonify({"status": "success", "content": "rebooted"}), 200
+    else:
+        return jsonify({"status": "error", "message": "File not found"}), 404
+
+@app.route('/api/shutdown', methods=['POST'])
+def shutdown():
+    if 1:
+        print("shutdown")
+        os.system('sudo shutdown -h now')
+        
+        return jsonify({"status": "success", "content": "shutdown"}), 200
+    else:
+        return jsonify({"status": "error", "message": "File not found"}), 404   
+     
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
