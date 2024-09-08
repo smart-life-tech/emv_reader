f3cc528aad7f9783a0b0673abdc3b6120f12d1c9
smart-life-tech <christlightworld@gmail.com>
2024-09-08 17:03:29 +0100

    modified:   magstripe.py

--- magstripe.py
+++ magstripe.py
@@ -8,10 +8,11 @@ DEVICE_PATH = "/dev/input/event1"
 # Event format: struct with 16-byte size
 EVENT_FORMAT = 'llHHI'
 EVENT_SIZE = struct.calcsize(EVENT_FORMAT)
-
+count=0
 # Map key codes to characters (normal and shifted)
 KEY_MAP = {
     2: '1', 3: '2', 4: '3', 5: '4', 6: '5', 7: '6', 8: '7', 9: '8', 10: '9', 11: '0',
+    12: '-', 13: '=',  # Added '=' to the map
     16: 'q', 17: 'w', 18: 'e', 19: 'r', 20: 't', 21: 'y', 22: 'u', 23: 'i', 24: 'o', 25: 'p',
     30: 'a', 31: 's', 32: 'd', 33: 'f', 34: 'g', 35: 'h', 36: 'j', 37: 'k', 38: 'l',
     44: 'z', 45: 'x', 46: 'c', 47: 'v', 48: 'b', 49: 'n', 50: 'm',
@@ -19,6 +20,7 @@ KEY_MAP = {
     # With shift
     'SHIFTED': {
         2: '!', 3: '@', 4: '#', 5: '$', 6: '%', 7: '^', 8: '&', 9: '*', 10: '(', 11: ')',
+        12: '_', 13: '+',  # Shifted characters for '-' and '='
         16: 'Q', 17: 'W', 18: 'E', 19: 'R', 20: 'T', 21: 'Y', 22: 'U', 23: 'I', 24: 'O', 25: 'P',
         30: 'A', 31: 'S', 32: 'D', 33: 'F', 34: 'G', 35: 'H', 36: 'J', 37: 'K', 38: 'L',
         44: 'Z', 45: 'X', 46: 'C', 47: 'V', 48: 'B', 49: 'N', 50: 'M',
@@ -64,8 +66,11 @@ def read_input_events(device_path):
 
                     # Stop capturing when '?' is detected
                     if captured_data.endswith('?'):
-                        print("\nCaptured Data: ", captured_data)
-                        captured_data = ""  # Reset the data for the next swipe
+                        count=count+1
+                        if count==2:
+                            print("\nCaptured Data: ", captured_data)
+                            captured_data = ""  # Reset the data for the next swipe
+                            count=0
 
 if __name__ == "__main__":
     # Make sure the script is run with enough privileges (root) to access the device
