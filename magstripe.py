7976a549f0d9531a169f00188f225f61772b34ce
smart-life-tech <christlightworld@gmail.com>
2024-09-08 16:50:08 +0100

    modified:   magstripe.py

--- magstripe.py
+++ magstripe.py
@@ -11,7 +11,6 @@ EVENT_SIZE = struct.calcsize(EVENT_FORMAT)
 
 # Map key codes to characters (normal and shifted)
 KEY_MAP = {
-    # Without shift
     2: '1', 3: '2', 4: '3', 5: '4', 6: '5', 7: '6', 8: '7', 9: '8', 10: '9', 11: '0',
     16: 'q', 17: 'w', 18: 'e', 19: 'r', 20: 't', 21: 'y', 22: 'u', 23: 'i', 24: 'o', 25: 'p',
     30: 'a', 31: 's', 32: 'd', 33: 'f', 34: 'g', 35: 'h', 36: 'j', 37: 'k', 38: 'l',
@@ -23,17 +22,33 @@ KEY_MAP = {
         16: 'Q', 17: 'W', 18: 'E', 19: 'R', 20: 'T', 21: 'Y', 22: 'U', 23: 'I', 24: 'O', 25: 'P',
         30: 'A', 31: 'S', 32: 'D', 33: 'F', 34: 'G', 35: 'H', 36: 'J', 37: 'K', 38: 'L',
         44: 'Z', 45: 'X', 46: 'C', 47: 'V', 48: 'B', 49: 'N', 50: 'M',
-        39: ':', 40: '+', 51: '<', 52: '>', 53: '?', 57: ' ', 12: 'B', 13: '^', 43: '?'
+        39: ':', 40: '+', 51: '<', 52: '>', 53: '?', 57: ' ',
     }
 }
 
 SHIFT_KEY_CODES = {42, 54}  # Left shift and right shift
+SHIFT_HELD = False
+
+def process_key_event(code, value):
+    global SHIFT_HELD
+
+    if value == 1:  # Key press
+        if code in SHIFT_KEY_CODES:
+            SHIFT_HELD = True
+        else:
+            if SHIFT_HELD and code in KEY_MAP['SHIFTED']:
+                return KEY_MAP['SHIFTED'][code]
+            elif code in KEY_MAP:
+                return KEY_MAP[code]
+    elif value == 0:  # Key release
+        if code in SHIFT_KEY_CODES:
+            SHIFT_HELD = False
+    return None
 
 def read_input_events(device_path):
     with open(device_path, 'rb') as device:
         print(f"Listening for keyboard events on {device_path}...")
         captured_data = ""
-        shift_pressed = False
 
         while True:
             # Read an event
@@ -41,22 +56,8 @@ def read_input_events(device_path):
             if event:
                 (tv_sec, tv_usec, type, code, value) = struct.unpack(EVENT_FORMAT, event)
 
-                # Handle Shift key press/release
-                if type == 1 and code in SHIFT_KEY_CODES:
-                    if value == 1:  # Shift pressed
-                        shift_pressed = True
-                    elif value == 0:  # Shift released
-                        shift_pressed = False
-
-                # Key press events (type 1)
-                elif type == 1 and value == 1:  # Key press
-                    if shift_pressed and code in KEY_MAP['SHIFTED']:
-                        key = KEY_MAP['SHIFTED'][code]
-                    elif code in KEY_MAP:
-                        key = KEY_MAP[code]
-                    else:
-                        key = ''
-
+                if type == 1:  # Key press/release event
+                    key = process_key_event(code, value)
                     if key:
                         captured_data += key
                         print(key, end='', flush=True)
