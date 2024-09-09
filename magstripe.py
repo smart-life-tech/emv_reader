abbc92a1ac8fba4683beb215f5485a34e92ec33c
smart-life-tech <christlightworld@gmail.com>
2024-09-09 09:03:17 +0100

    modified:   magstripe.py

--- magstripe.py
+++ magstripe.py
@@ -1,7 +1,8 @@
 import os
 import struct
 from datetime import datetime
-
+import pychrome
+import time
 # The path to your keyboard event file
 DEVICE_PATH = "/dev/input/event1"
 
@@ -31,6 +32,46 @@ KEY_MAP = {
 SHIFT_KEY_CODES = {42, 54}  # Left shift and right shift
 SHIFT_HELD = False
 
+def chrome(card_data,type):
+    print("Starting Chrome interaction")
+    try:
+        # Connect to the Chromium browser
+        browser = pychrome.Browser(url="http://127.0.0.1:9222")
+        tabs = browser.list_tab()
+
+        if not tabs:
+            print("No tabs found")
+            exit(1)
+
+        tab = tabs[0]
+        tab.start()
+        # Define the path to the secret file
+        secret_file_path = os.path.expanduser("~/.hidden_dir/secret_file.txt")
+
+        # Read the file contents
+        with open(secret_file_path, 'r') as file:
+            secret_data = file.read()
+
+        # Split the secret data into pos_id and brn
+        secret_lines = secret_data.strip().splitlines()
+        pos_id = secret_lines[0].split(': ')[1]
+        brn = secret_lines[1].split(': ')[1]
+        print(brn)
+        print(pos_id)
+
+        # JavaScript code to trigger the card check with simulated card data
+        js_code = f"""
+        window.emvProcessed("{card_data}", "{type}", "{pos_id}", "{brn}");
+        """
+        # Execute the JavaScript code
+        result = tab.Runtime.evaluate(expression=js_code)
+        print("JavaScript executed:", result)
+        
+        # Optional: Close the tab connection
+        time.sleep(1)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        
 def process_key_event(code, value):
     global SHIFT_HELD
 
@@ -71,6 +112,8 @@ def read_input_events(device_path):
                         print(count)
                         if count==4:
                             print("\nCaptured Data: ", captured_data)
+                            time.sleep(1)
+                            chrome(captured_data,"swipe")
                             captured_data = ""  # Reset the data for the next swipe
                             count=0
 
