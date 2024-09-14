ccfc28253d2222824a8c385a5c7ea1b12013f645
smart-life-tech <christlightworld@gmail.com>
2024-09-14 01:01:45 +0100

    modified:   internet.sh

--- internet.sh
+++ internet.sh
@@ -12,9 +12,13 @@
 #wait 
 #sleep 1
 
+
+python /home/chingup/emv_reader/pic.py &
 # Check if internet is available by pinging google.com (no https://)
 wget -q --spider http://google.com 
-
+sleep 5
+# Check the exit status of the ping command
+# If the exit status is 0, internet is available
 if [ $? -eq 0 ]; then
     echo "Internet is available" >> /home/chingup/internet_check.log
     # Open the online page
