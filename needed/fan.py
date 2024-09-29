c68fd94028534d5c16b33ba05f2c0194954b7683
smart-life-tech <christlightworld@gmail.com>
2024-09-29 12:29:12 +0100

    modified:   needed/fan.py

--- needed/fan.py
+++ needed/fan.py
@@ -2,9 +2,9 @@ import serial
 import time
 import RPi.GPIO as GPIO
 import os
-
+import re
 # UART Setup (modify as per your battery module documentation)
-uart_port = '/dev/serial0'  # This should match the UART port for your battery
+uart_port = '/dev/serial1'  # This should match the UART port for your battery
 baud_rate = 9600
 
 # Serial object, initialized to None
@@ -25,8 +25,63 @@ FAN_PIN = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(FAN_PIN, GPIO.OUT)
 
+class UPS2:
+    def __init__(self,port):
+        self.ser  = serial.Serial(port,9600)        
+        
+    def get_data(self,nums):
+        while True:
+            self.count = self.ser.inWaiting()
+            
+            if self.count !=0:
+                self.recv = self.ser.read(nums)
+                return self.recv
+    
+    def decode_uart(self):
+        self.uart_string = self.get_data(100)
+#    print(uart_string)
+        self.data = self.uart_string.decode('ascii','ignore')
+#    print(data)
+        self.pattern = r'\$ (.*?) \$'
+        self.result = re.findall(self.pattern,self.data,re.S)
+    
+        self.tmp = self.result[0]
+    
+        self.pattern = r'SmartUPS (.*?),'
+        self.version = re.findall(self.pattern,self.tmp)
+    
+        self.pattern = r',Vin (.*?),'
+        self.vin = re.findall(self.pattern,self.tmp)
+        
+        self.pattern = r'BATCAP (.*?),'
+        self.batcap = re.findall(self.pattern,self.tmp)
+        
+        self.pattern = r',Vout (.*)'
+        self.vout = re.findall(self.pattern,self.tmp)
+
+        return self.version[0],self.vin[0],self.batcap[0],self.vout[0]
+    
 # Function to read battery data over UART
 def read_battery_data():
+    i=1
+    print("This is UPS v2 class file")
+    test = UPS2("/dev/serial0")
+    version,vin,batcap,vout = test.decode_uart()
+    print("--------------------------------")
+    print("       UPS Version:"+version)
+    print("--------------------------------")
+    version,vin,batcap,vout = test.decode_uart()
+    print("-%s-" %i)
+       
+    if vin == "NG":
+        print("USB input adapter : NOT connected!")
+    else:
+        print("USB input adapter : connected!")
+    print("Battery Capacity: "+batcap+"%")
+    print("UPS Output Voltage: "+vout+" mV")
+    print("\n")
+       
+   
     if ser is None:
         print("Serial port is not available, skipping battery read.")
         return None
